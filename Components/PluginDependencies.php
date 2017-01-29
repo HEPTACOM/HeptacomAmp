35b09defe69e3dc0d8d944c59c59ee1eb5c271f8
Joshua Behrens <behrens@heptacom.de>
2017-01-29 07:59:22 +0100

    Added component to detect dependencies in a readable manner

---
+++ Components/PluginDependencies.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace HeptacomAmp\Components;
+
+use HeptacomAmp\Components\PluginDependencies\Dependency;
+
+/**
+ * Class PluginDependencies
+ * @package HeptacomAmp\Components
+ */
+class PluginDependencies
+{
+    private function __construct()
+    {
+        if (extension_loaded('dom')) {
+            $this->dependencies[] = new Dependency('php DOM Extension', '*', phpversion('dom'), true);
+        } else {
+            $this->dependencies[] = new Dependency('php DOM Extension', '*', '', false);
+        }
+    }
+
+    /**
+     * @var PluginDependencies
+     */
+    private static $instance;
+
+    /**
+     * Returns the singleton.
+     * @return PluginDependencies The singleton.
+     */
+    public static function instance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new PluginDependencies();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * @var Dependency[]
+     */
+    private $dependencies = [];
+
+    /**
+     * Returns all dependencies.
+     * @return PluginDependencies\Dependency[]
+     */
+    public function getDependencies()
+    {
+        return $this->dependencies;
+    }
+};
