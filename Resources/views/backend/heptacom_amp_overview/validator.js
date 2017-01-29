03182eb9592d2672bf242b952c514a1c70d7e53e
Joshua Behrens <behrens@heptacom.de>
2017-01-29 19:50:33 +0100

    Added function to request google cache request

--- Resources/views/backend/heptacom_amp_overview/validator.js
+++ Resources/views/backend/heptacom_amp_overview/validator.js
@@ -10,5 +10,15 @@ heptacom = {
                 (data.status == 'PASS' ? success : error)(amp.validator.validateString(data))
             }
         });
+    },
+
+    googleAmpCache: function(url) {
+        if (url.indexOf('https://') === 0) {
+            url = 'https://cdn.ampproject.org/c/s/' + url.substring(8);
+        } else if (url.indexOf('http://')) {
+            url = 'https://cdn.ampproject.org/c/' + url.substring(7);
+        }
+
+        return $.get({url: url});
     }
 };
