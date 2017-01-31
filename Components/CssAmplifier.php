9d6bed56962833e28b6a15a7759c4c872b562b8f
Julian Krzefski <krzefski@heptacom.de>
2017-01-31 04:35:42 +0100

    Started to implement css processing.

--- Components/CssAmplifier.php
+++ Components/CssAmplifier.php
@@ -2,6 +2,19 @@
 
 namespace HeptacomAmp\Components;
 
+use Sabberworm\CSS\Parser;
+use Sabberworm\CSS\OutputFormat;
+use Sabberworm\CSS\CSSList\Document;
+use Sabberworm\CSS\CSSList\CSSList;
+use Sabberworm\CSS\Property\Selector;
+use Sabberworm\CSS\Rule\Rule;
+use Sabberworm\CSS\RuleSet\DeclarationBlock;
+use Sabberworm\CSS\RuleSet\RuleSet;
+use Sabberworm\CSS\Value\CSSString;
+use Sabberworm\CSS\Value\URL;
+use Sabberworm\CSS\Value\Value;
+use Sabberworm\CSS\CSSList\AtRuleBlockList;
+
 class CssAmplifier
 {
     /**
@@ -10,13 +23,95 @@ class CssAmplifier
     protected $css;
 
     /**
+     * @var Document
+     */
+    protected $parser;
+
+    /**
      * @param $css
      * @return string
      */
     public function processCss($css)
     {
         $this->css = $css;
+        $parser = new Parser($this->css);
+        $this->parser = $parser->parse();
 
-        return $this->css;
+        $this->removeMediaQueries();
+        $this->rewritePaths();
+        $this->removeDisallowedStyles();
+
+        return $this->parser->render(OutputFormat::createCompact());
+    }
+
+    protected function removeMediaQueries()
+    {
+        /** @var CSSList[] $contents */
+        $contents = $this->parser->getContents();
+
+        foreach ($contents as $list) {
+            if (!($list instanceof AtRuleBlockList)) {
+                continue;
+            }
+            /** @var AtRuleBlockList $list */
+            if ($list->atRuleName() != 'media') {
+                continue;
+            }
+            if (strpos($list->atRuleArgs(), 'min-width') !== false) {
+                $this->parser->remove($list);
+            }
+        }
+    }
+
+    protected function rewritePaths()
+    {
+        /** @var Value[] $values */
+        $values = $this->parser->getAllValues();
+
+        foreach ($values as $value) {
+            if (!($value instanceof URL)) {
+                continue;
+            }
+            /** @var URL $value */
+            $origPath = trim($value->getURL(), '\"');
+            $origPath = array_shift(explode('?', $origPath));
+
+            $path = realpath(implode(DIRECTORY_SEPARATOR, [Shopware()->DocPath(), 'web', 'cache', $origPath]));
+            $path = implode(DIRECTORY_SEPARATOR, [
+                Shopware()->Front()->Request()->getBaseUrl(),
+                substr($path, strlen(Shopware()->DocPath()))
+            ]);
+
+            $value->setURL(new CSSString($path));
+        }
+    }
+
+    protected function removeDisallowedStyles()
+    {
+        /** @var RuleSet[] $ruleSets */
+        $ruleSets = $this->parser->getAllRuleSets();
+        foreach ($ruleSets as $ruleSet) {
+            if (!($ruleSet instanceof DeclarationBlock)) {
+                continue;
+            }
+            /** @var DeclarationBlock $ruleSet */
+
+            /** @var Rule[] $rules */
+            $rules = $ruleSet->getRules();
+            foreach ($rules as $rule) {
+                $rule->setIsImportant(false);
+
+                /** @var Selector[] $selectors */
+                $selectors = $ruleSet->getSelectors();
+                $regex = '/(?=(\*)(?!=))/';
+                foreach ($selectors as $selector) {
+                    preg_match_all($regex, $selector->getSelector(), $matches);
+
+                    if ($matches[1][0] == '*') {
+                        $ruleSet->removeRule($rule);
+                    }
+                }
+            }
+        }
     }
 }
