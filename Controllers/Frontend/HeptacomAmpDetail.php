145b31ae003d8472189c6dbaa3f476fa275ef46f
Julian Krzefski <krzefski@heptacom.de>
2017-02-13 06:07:03 +0100

    Added SEO-redirect from old controller paths to new structure.

--- Controllers/Frontend/HeptacomAmpDetail.php
+++ Controllers/Frontend/HeptacomAmpDetail.php
@@ -2,6 +2,22 @@
 
 class Shopware_Controllers_Frontend_HeptacomAmpDetail extends Shopware_Controllers_Frontend_Detail
 {
+    public function preDispatch()
+    {
+        if ($this->Request()->getParam('heptacom_amp_redirect')) {
+            if ($this->Request()->getActionName() == 'index') {
+                $this->redirect([
+                    'controller' => 'detail',
+                    'action' => 'index',
+                    'sArticle' => (int)$this->Request()->get('sArticle'),
+                    'amp' => 1,
+                ]);
+            }
+        }
+
+        parent::preDispatch();
+    }
+
     public function indexAction()
     {
         $this->View()->loadTemplate(implode(DIRECTORY_SEPARATOR, ['frontend', 'plugins', 'heptacom_amp', 'detail', 'index.tpl']));
