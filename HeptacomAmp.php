f05ea0c52d14cf4fa4f8d227c0ffe73605a153cf
Julian Krzefski <krzefski@heptacom.de>
2017-02-15 08:57:42 +0100

    Added checkLicense() method.

--- HeptacomAmp.php
+++ HeptacomAmp.php
@@ -3,11 +3,13 @@
 namespace HeptacomAmp;
 
 use Enlight_Event_EventArgs;
+use Exception;
 use Shopware\Components\Plugin;
 use Shopware\Components\Plugin\Context\ActivateContext;
 use Shopware\Components\Plugin\Context\DeactivateContext;
 use Shopware\Components\Plugin\Context\InstallContext;
 use Shopware\Components\Plugin\Context\UpdateContext;
+use Shopware_Components_License;
 
 class HeptacomAmp extends Plugin
 {
@@ -32,7 +34,17 @@ class HeptacomAmp extends Plugin
      */
     public function update(UpdateContext $context)
     {
+        $this->checkLicense();
         $context->scheduleClearCache(InstallContext::CACHE_LIST_ALL);
+    }
+
+    /**
+     * @param InstallContext $context
+     */
+    public function install(InstallContext $context)
+    {
+        $this->checkLicense();
+        parent::install($context);
     }
 
     /**
@@ -51,5 +63,49 @@ class HeptacomAmp extends Plugin
     public function autoloadComposer(Enlight_Event_EventArgs $args)
     {
         require_once implode(DIRECTORY_SEPARATOR, [$this->getPath(), 'vendor', 'autoload.php']);
+    }
+
+    /**
+     * checkLicense()-method for HeptacomAmp
+     *
+     * @param bool $throwException
+     * @return bool|string
+     * @throws Exception
+     */
+    public function checkLicense($throwException = true)
+    {
+        try {
+            /** @var $l Shopware_Components_License */
+            $l = Shopware()->License();
+        } catch (Exception $e) {
+            if ($throwException) {
+                throw new Exception('The license manager has to be installed and active');
+            } else {
+                return false;
+            }
+        }
+
+        try {
+            static $r, $module = 'HeptacomAmp';
+            if(!isset($r)) {
+                $s = base64_decode('uZv6Qx9VMVu7Arx62pFWKkXIPpg=');
+                $c = base64_decode('FtdAlOEwfWjkSxNe18BfbPOi1zQ=');
+                $r = sha1(uniqid('', true), true);
+                $i = $l->getLicense($module, $r);
+                $t = $l->getCoreLicense();
+                $u = strlen($t) === 20 ? sha1($t . $s . $t, true) : 0;
+                $r = $i === sha1($c. $u . $r, true);
+            }
+            if (!$r && $throwException) {
+                throw new Exception('License check for module "' . $module . '" has failed.');
+            }
+            return $r;
+        } catch (Exception $e) {
+            if ($throwException) {
+                throw new Exception('License check for module "' . $module . '" has failed.');
+            } else {
+                return false;
+            }
+        }
     }
 }
