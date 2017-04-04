f677d27b15e8119be8da53df0796db4812a2b577
Julian Krzefski <krzefski@heptacom.de>
2017-04-04 02:51:07 +0200

    Implemented a simple loop to request amp pages for some article ids.

--- Commands/CacheWarmer.php
+++ Commands/CacheWarmer.php
@@ -4,6 +4,12 @@ namespace HeptacomAmp\Commands;
 
 use HeptacomAmp\Components\DispatchSimulator;
 use Shopware\Commands\ShopwareCommand;
+use Shopware\Components\Model\ModelManager;
+use Shopware\Models\Category\Category;
+use Shopware\Models\Category\Repository as CategoryRepository;
+use Shopware\Models\Shop\Repository as ShopRepository;
+use Shopware\Models\Shop\Shop;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -20,5 +26,36 @@ class CacheWarmer extends ShopwareCommand
     {
         /** @var DispatchSimulator $dispatchSimulator */
         $dispatchSimulator = $this->container->get('heptacom_amp.components.dispatch_simulator');
+
+        /** @var ModelManager $modelManager */
+        $modelManager = $this->container->get('Models');
+
+        /** @var ShopRepository $shopRepository */
+        $shopRepository = $modelManager->getRepository(Shop::class);
+
+        /** @var Shop[] $shops */
+        $shops = $shopRepository->findBy(['active' => true]);
+
+        /** @var CategoryRepository $categoryRepository */
+        $categoryRepository = $modelManager->getRepository(Category::class);
+
+        foreach ($shops as $shop) {
+            $articleIds = $this->getArticleIds();
+
+            $progress = new ProgressBar($output, count($articleIds));
+
+            foreach ($articleIds as $articleId) {
+                $dispatchSimulator->request($shop->getId(), ['controller' => 'detail', 'sArticle' => $articleId]);
+                $progress->advance();
+            }
+
+            $progress->finish();
+            $output->writeln('');
+        }
+    }
+
+    private function getArticleIds()
+    {
+        return range(1, 10);
     }
 }
