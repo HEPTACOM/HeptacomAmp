a60a4342415bb5895a6626b8745dbc5a543e0536
Joshua Behrens <behrens@ksk-agentur.de>
2017-07-31 21:41:00 +0200

    Implemented dependency checking

--- Resources/views/backend/_resources/KskAmp/BackendApplication/js/views/DependencyOverview.vue
+++ Resources/views/backend/_resources/KskAmp/BackendApplication/js/views/DependencyOverview.vue
@@ -20,15 +20,47 @@
                 </ul>
             </li>
         </navigation>
+        <table class="uk-table uk-table-striped uk-table-responsive">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Erforderliche Version</th>
+                    <th>Installierte Version</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="dependency in dependencies">
+                    <td>
+                        <i class="fa fa-check-circle uk-text-success" v-if="dependency.valid"></i>
+                        <i class="fa fa-exclamation-triangle uk-text-danger" v-if="!dependency.valid"></i>
+                        &nbsp;
+                        {{dependency.name}}
+                    </td>
+                    <td>
+                        {{dependency.version.required}}
+                    </td>
+                    <td>
+                        {{dependency.version.current}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script type="application/javascript">
+    import KskAmpBackend from '../lib/KskAmpBackend.js';
     import Navigation from '../components/navigation.vue';
 
     export default {
         components: {
             Navigation
+        },
+        data: () => ({
+            dependencies: []
+        }),
+        created: function () {
+            KskAmpBackend.dependencies.then(response => this.dependencies = response.data.data);
         }
     }
 </script>
