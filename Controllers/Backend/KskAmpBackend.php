6f81c94d1d9dd86b28d46862dbd3e629cd86f709
Joshua Behrens <behrens@ksk-agentur.de>
2017-08-02 02:12:35 +0200

    Added JSON request to access the category tree

--- Controllers/Backend/KskAmpBackend.php
+++ Controllers/Backend/KskAmpBackend.php
@@ -2,6 +2,8 @@
 
 use HeptacomAmp\Components\PluginDependencies;
 use Shopware\Components\CSRFWhitelistAware;
+use Shopware\Models\Category\Category;
+use Shopware\Models\Category\Repository;
 
 /**
  * Class Shopware_Controllers_Backend_KskAmpBackend
@@ -17,6 +19,7 @@ class Shopware_Controllers_Backend_KskAmpBackend extends Shopware_Controllers_Ap
     {
         return [
             'getDependencies',
+            'getCategories',
         ];
     }
 
@@ -36,6 +39,45 @@ class Shopware_Controllers_Backend_KskAmpBackend extends Shopware_Controllers_Ap
         $this->View()->assign([
             'success' => false,
             'data' => $this->getDependencies(),
+        ]);
+    }
+
+    /**
+     * @param Category $category
+     * @return array
+     */
+    private function hydrateCategory(Category $category)
+    {
+        $result = [
+            'id' => $category->getId(),
+            'name' => $category->getName(),
+        ];
+
+        if (!$category->getChildren()->isEmpty()) {
+            $result['categories'] = array_map([$this, 'hydrateCategory'], $category->getChildren()->toArray());
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    private function getCategories()
+    {
+        /** @var Repository $categoryRepository */
+        $categoryRepository = $this->getModelManager()->getRepository(Category::class);
+        return array_map([$this, 'hydrateCategory'], $categoryRepository->findBy(['parent' => null]));
+    }
+
+    /**
+     * Callable via /backend/KskAmpBackend/getCategories
+     */
+    public function getCategoriesAction()
+    {
+        $this->View()->assign([
+            'success' => false,
+            'data' => $this->getCategories(),
         ]);
     }
 }
--- Resources/views/backend/_resources/KskAmp/BackendApplication/js/lib/KskAmpBackend.js
+++ Resources/views/backend/_resources/KskAmp/BackendApplication/js/lib/KskAmpBackend.js
@@ -3,5 +3,9 @@ import Axios from 'axios';
 export default {
     get dependencies() {
         return Axios.get('getDependencies');
+    },
+
+    get categories() {
+        return Axios.get('getCategories');
     }
 }
