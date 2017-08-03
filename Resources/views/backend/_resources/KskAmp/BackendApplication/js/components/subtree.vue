df30efadc821232a0d545c2633c4746da21b80a6
Joshua Behrens <behrens@ksk-agentur.de>
2017-08-03 20:30:35 +0200

    Added click event to tree items

--- Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/subtree.vue
+++ Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/subtree.vue
@@ -3,7 +3,7 @@
         <ul class="uk-nav-sub">
             <li v-for="child in data">
                 <span v-on:click="clickItem(child)">{{child[nameProperty]}}</span>
-                <sub-tree v-bind:data="child[childrenProperty]" v-bind:childrenProperty="childrenProperty" v-bind:nameProperty="nameProperty"></sub-tree>
+                <sub-tree v-bind:data="child[childrenProperty]" v-bind:childrenProperty="childrenProperty" v-bind:nameProperty="nameProperty" v-on:click-item="clickItem"></sub-tree>
             </li>
         </ul>
     </div>
@@ -16,6 +16,11 @@
             data: Array,
             childrenProperty: String,
             nameProperty: String
+        },
+        methods: {
+            clickItem(item) {
+                this.$emit('click-item', item);
+            }
         }
     }
 </script>
--- Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/tree.vue
+++ Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/tree.vue
@@ -3,7 +3,7 @@
         <ul class="uk-nav-default uk-nav-parent-icon" uk-nav="multiple: true">
             <li v-for="child in data">
                 <span v-on:click="clickItem(child)">{{child[nameProperty]}}</span>
-                <sub-tree v-bind:data="child[childrenProperty]" v-bind:childrenProperty="childrenProperty" v-bind:nameProperty="nameProperty"></sub-tree>
+                <sub-tree v-bind:data="child[childrenProperty]" v-bind:childrenProperty="childrenProperty" v-bind:nameProperty="nameProperty" v-on:click-item="clickItem"></sub-tree>
             </li>
         </ul>
     </div>
@@ -20,6 +20,11 @@
             data: Array,
             childrenProperty: String,
             nameProperty: String
+        },
+        methods: {
+            clickItem(item) {
+                this.$emit('click-item', item);
+            }
         }
     }
 </script>
