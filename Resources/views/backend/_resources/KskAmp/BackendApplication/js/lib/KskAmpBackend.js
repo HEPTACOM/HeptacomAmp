498a28ba7602b5c7ac7917a4b0d84579fb38a014
Joshua Behrens <behrens@ksk-agentur.de>
2017-08-04 17:52:35 +0200

    Load all articles on category load

--- Resources/views/backend/_resources/KskAmp/BackendApplication/js/lib/KskAmpBackend.js
+++ Resources/views/backend/_resources/KskAmp/BackendApplication/js/lib/KskAmpBackend.js
@@ -6,7 +6,27 @@ export default {
     },
 
     get categories() {
-        return Axios.get('getCategories');
+        let pullArticlesForCategory = (category) => {
+            category.articles = [];
+            this.getArticlesByShopCategory(1, category.id, p => category.articles.push(...p.data));
+
+            if (category.categories && category.categories.length) {
+                for (let subCategory of category.categories) {
+                    pullArticlesForCategory(subCategory);
+                }
+            }
+        };
+
+        let pullArticles = (request) => {
+            for (let category of request.data.data) {
+                pullArticlesForCategory(category);
+            }
+
+            return Promise.resolve(request);
+        };
+
+        return Axios.get('getCategories')
+            .then(pullArticles);
     },
 
     getArticlesPageByShopCategory(shop, category, lastArticle) {
