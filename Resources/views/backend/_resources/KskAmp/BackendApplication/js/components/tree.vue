8b3eb220364ed777d2ff14d2b87a3d4841e86e45
Joshua Behrens <behrens@ksk-agentur.de>
2017-08-04 13:06:17 +0200

    Removed unused component setting

--- Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/tree.vue
+++ Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/tree.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <ul class="uk-nav-default uk-nav-parent-icon" uk-nav="multiple: true">
+        <ul class="uk-nav-default uk-nav-parent-icon" uk-nav>
             <li v-for="child in data">
                 <span v-on:click="clickItem(child)">{{child[nameProperty]}}</span>
                 <sub-tree v-bind:data="child[childrenProperty]" v-bind:childrenProperty="childrenProperty" v-bind:nameProperty="nameProperty" v-on:click-item="clickItem"></sub-tree>
