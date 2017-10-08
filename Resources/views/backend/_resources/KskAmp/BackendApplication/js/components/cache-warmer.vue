da072624b45434449d510b1fb6095a2f6b28015e
Joshua Behrens <behrens@ksk-agentur.de>
2017-10-08 16:21:53 +0200

    Added fetching of article urls with error reporting

--- Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/cache-warmer.vue
+++ Resources/views/backend/_resources/KskAmp/BackendApplication/js/components/cache-warmer.vue
@@ -11,9 +11,28 @@
             </h3>
         </div>
         <div class="uk-card-body">
-            <p class="uk-text-meta uk-margin-remove-top">
-                <progress class="uk-progress uk-text-success" value="0" v-bind:max="items.length"></progress>
+            <p>
+                <progress class="uk-progress uk-text-success" v-bind:value="processed.length + errors.length" v-bind:max="items.length"></progress>
             </p>
+            <div v-if="errors.length" class="uk-margin-medium-top">
+                <h3>
+                    Fehler <span class="uk-label">{{errors.length}}</span>
+                </h3>
+                <ul class="uk-overflow-auto uk-height-medium" uk-accordion="">
+                    <li v-for="error in errors">
+                        <span class="uk-accordion-title">{{error.item.name}}</span>
+                        <div class="uk-accordion-content">
+                            <ul class="uk-iconnav">
+                                <li class="uk-text-danger" v-on:click="showError(error)"><f-a icon="exclamation-circle" v-bind:block="true"></f-a></li>
+                                <template v-for="link in error.item.urls">
+                                    <li><a v-bind:href="link.ampUrl" target="_blank"><f-a icon="bolt" v-bind:block="true"></f-a></a></li>
+                                    <li><a v-bind:href="link.canonicalUrl" target="_blank"><f-a icon="desktop" v-bind:block="true"></f-a></a></li>
+                                </template>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
         <div class="uk-card-footer uk-text-center">
             <button class="uk-button uk-button-primary" v-bind:disabled="loading || processing" v-on:click="startProcessing">
@@ -24,6 +43,8 @@
 </template>
 
 <script type="application/javascript">
+    import UIkit from 'uikit';
+    import KskAmpBackend from '../lib/KskAmpBackend';
     import FA from './font-awesome.vue';
 
     export default {
@@ -38,13 +59,45 @@
         data: () => ({
             processing: false,
             processed: [],
+            errors: [],
             selectedIndex: 0,
         }),
         methods: {
             startProcessing() {
                 this.processing = true;
                 this.processed = [];
+                this.errors = [];
                 this.selectedIndex = 0;
+                this.fetch();
+            },
+            fetch() {
+                if (this.selectedIndex < this.items.length) {
+                    KskAmpBackend.callHeadUrls(this.items[this.selectedIndex].urls.map(p => p.ampUrl))
+                        .then(
+                            this.processed.push(this.items[this.selectedIndex]),
+                            p => {
+                                this.errors.push({
+                                    item: this.items[this.selectedIndex],
+                                    exception: p
+                                });
+                            }
+                        ).then(() => {
+                            this.selectedIndex++;
+                            return this.fetch();
+                        });
+                } else {
+                    this.processing = false;
+                }
+            },
+            showError(item) {
+                UIkit.modal.alert(JSON.stringify({
+                    config: item.exception.config,
+                    headers: item.exception.headers,
+                    method: item.exception.method,
+                    url: item.exception.url,
+                    data: item.exception.data,
+                    response: item.exception.response
+                }));
             }
         }
     }
